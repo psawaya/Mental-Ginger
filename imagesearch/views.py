071cc2d50849893294c5c34441c03f4a46bacc6d
Gleb <glebk@glebk-ubuntu.(none)>
2011-04-09 18:28:16 -0700

    Wired up image search

--- imagesearch/views.py
+++ imagesearch/views.py
@@ -8,16 +8,19 @@ flickrAPIKey = 'b6e189ca8d6c0e505a32d67dc846c06c'
 flickrAPISecret = '988479871ee728f9'
 
 def searchHandler(request):
+    # Construct flickr api object.
     flickr = GaeFlickrLib(api_key = flickrAPIKey,
                           api_secret = flickrAPISecret)
 
-    #tags = self.request.get('tags')
-    #if tags is None: return
-    #logging.debug("TAGS: %s " % self.request.get('tags'))
-    #results = flickr.photos_search(tags = tags)
-    #    self.response.out.write(template.render('templates/images.html',{'images' : [r.url() for r in results]}))
+    # Query flickr.
+    if request.GET:
+        tags = request.GET['tags']
+        if tags is None:
+            c = Context({})
+        #logging.debug("TAGS: %s " % self.request.get('tags'))
+        results = flickr.photos_search(tags = tags)
+        c = Context({'images' : [r.url() for r in results]})
 
+    # Render template and return response.
     t = loader.get_template('imagesearch/images.html')
-    #c = Context({'images' : [r.url() for r in results]})
-    c = Context({})
     return HttpResponse(t.render(c))
